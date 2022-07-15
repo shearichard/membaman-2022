edd76521d2f49fd1a28db98ee24ae665ab4c9675
Richard Shea <rshea@thecubagroup.com>
2022-07-15 20:14:55 +1200

    Add references to members and fees apps in INSTALLED_APPS

--- membaman/membaman/membaman/settings/base.py
+++ membaman/membaman/membaman/settings/base.py
@@ -29,16 +29,24 @@ ALLOWED_HOSTS = []
 
 
 # Application definition
-
-INSTALLED_APPS = [
+DJANGO_APPS = (
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-]
-
+)
+#
+THIRD_PARTY_APPS = ()
+#
+LOCAL_APPS = (
+    'members',
+    'fees',
+)
+#
+INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
+#####################################################################
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
