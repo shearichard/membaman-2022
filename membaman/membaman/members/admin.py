d761252324514b97ae80c3bbba249adaaad6cf64
Richard Shea <rshea@thecubagroup.com>
2022-07-15 20:46:59 +1200

    Expose members models to admin

--- membaman/membaman/members/admin.py
+++ membaman/membaman/members/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Organisation
+from .models import SubOrganisation
+from .models import Family
+#from .models import Person
+from .models import Caregiver
+from .models import Member
+
+
+admin.site.register(Organisation)
+admin.site.register(SubOrganisation)
+admin.site.register(Family)
+#admin.site.register(Person)
+admin.site.register(Caregiver)
+admin.site.register(Member)
