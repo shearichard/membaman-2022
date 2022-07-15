b9c343e42bb6ae32069530e864c1216bb0662d3b
Richard Shea <rshea@thecubagroup.com>
2022-07-15 20:46:40 +1200

    Fix up fees model exposure to admin

--- membaman/membaman/fees/admin.py
+++ membaman/membaman/fees/admin.py
@@ -6,3 +6,11 @@ from .models import AccountEntry
 from .models import AccountDebt
 from .models import AccountPayment
 from .models import ReferenceMapper
+
+admin.site.register(Year)
+admin.site.register(SubYear)
+admin.site.register(Income)
+admin.site.register(AccountEntry)
+admin.site.register(AccountDebt)
+admin.site.register(AccountPayment)
+admin.site.register(ReferenceMapper)
