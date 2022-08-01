b0e2ee09256b21f67a5629a0e08bf86c2e0c4c3d
Richard Shea <rshea@thecubagroup.com>
2022-08-01 18:19:11 +1200

    Clean a lot of commented code out of membaman.urls

--- membaman/membaman/membaman/urls.py
+++ membaman/membaman/membaman/urls.py
@@ -20,35 +20,6 @@ from django.views.generic.base import TemplateView
 admin.site.site_header = 'MembaMan Administration Area'
 admin.site.site_title = 'MembaMan Admin'
 
-#app_name = 'membaman'
-
-
-# The following used to be in urlpatterns but I can no longer have it
-# in there withough generating an error. Temporrily I'm moving out here.
-#
-#re_path(r'^membaman/', include('membaman.urls')),
-#re_path(r'^membaman/', include('membaman.urls', namespace="membaman")),
-#
-#
-
-
-'''
-urlpatterns = [ 
-    re_path(r'^$', TemplateView.as_view(template_name='index.html'), name='index'),
-    re_path(r'^fees/', include('fees.urls', namespace="fees")),
-    re_path(r'^members/', include('members.urls', namespace="members")),
-    re_path(r'^admin/', admin.site.urls),
-]
-'''
-'''
-urlpatterns = [ 
-    re_path(r'^$', TemplateView.as_view(template_name='index.html'), name='index'),
-    re_path(r'^fees/', include('fees.urls')),
-    re_path(r'^members/', include('members.urls')),
-    re_path(r'^admin/', admin.site.urls),
-]
-'''
-    #re_path(r'^membaman/', include('membaman.urls', 'membaman')),
 urlpatterns = [ 
     re_path(r'^$', TemplateView.as_view(template_name='index.html'), name='membamanindex'),
     re_path(r'^fees/', include('fees.urls', 'fees')),
